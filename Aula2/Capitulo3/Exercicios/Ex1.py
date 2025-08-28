9110de0e5e901e6bd3833fd080294253530a3848
Leandro Teixeira <leandro7teixeita@gmail.com>
2025-08-28 11:11:28 -0300

    Delete Aula2/Capitulo3 directory

--- Aula2/Capitulo3/Exercicios/Ex1.py
+++
@@ -1,6 +0,0 @@
-colocacao = ["Real Madrid", "Barcelona", "Atlético de Madrid", "Sevilla", "Valencia"]
-
-print("Top 3:", colocacao[:3])
-print("2 últimos:", colocacao[-2:])
-print("Ordem alfabética:", sorted(colocacao))
-print(colocacao.index("Barcelona"))
--- Aula2/Capitulo3/Exercicios/Ex2.py
+++
@@ -1,5 +0,0 @@
-loja1 = {"Iphone", "Motorola", "Samsung", "Nokia"}
-loja2 = {"Iphone", "Samsung", "LG"}
-
-print("Modelos disponíveis nas duas lojas:", loja1 | loja2)
-print("Modelo disponíveis em ambas as lojas:", loja1 & loja2)
